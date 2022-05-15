892d344b7a94253fe62836d3ea2db6a06005474f
Tamil Selvan <tamil777selvan@gmail.com>
2022-05-15 13:05:32 +0530

    Typescript (#5)
    * Converting to Typescript
    
    * Adding workflow
    
    Co-authored-by: tamil.natesan <tamil.n40@wipro.com>

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # wdio-testrecorder-reporter
-A reporter for [WDIO v5](https://webdriver.io/) that generates videos of your wdio test executions.
+A reporter for [WDIO](https://webdriver.io/) that generates videos of your wdio cucumber test executions.
 
 This reporter uses [ffmpeg](https://www.ffmpeg.org/) to convert sequence of images to video.
 ## Installation
@@ -18,28 +18,14 @@ Configure the output directory in your wdio.conf.js file:
 ```js
 exports.config = {
     // ...
-    reporters: ['testRecorder', {videoOutputPath: '', savePassedVideo: false, jsonWireCommands: '', framerate: ''}]
+    reporters: ['TestRecorder', {videoOutputPath: '', attachVideoToCucumberReport: false}]
   // ...
 }
 ```
 
-- `videoOutputPath` - Output folder path to store reports.
-- `savePassedVideo` - Enable video storage for passed tests.
-- `jsonWireCommands` - Optional. Defaults to `default/wdioCommand.json`. 
-- `framerate` - Optional, range varies from 1 to 50. Defaults to `3.5`. 
+- `videoOutputPath` - Output folder path to store video reports.
+- `attachVideoToCucumberReport` - Attach the recorded video to cucumber reports.
+- `removeAttachedVideos` - Delete the saved video from temp file
 
-## Cucumber JSON Report Integration
-Below method can be used to attach saved video to cucumber json reports. Installation and implementation of `wdio-cucumberjs-json-reporter` can be found [here](https://www.npmjs.com/package/wdio-cucumberjs-json-reporter)
-
-```js
-const fs = require('fs');
-const path = require('path');
-const cucumberJson = require('wdio-cucumberjs-json-reporter').default;
-
-let videoFilePath = fs.readFileSync(path.resolve(`${videoOutputPath}/video/${scenarioName}.mp4`));
-let base64Video = new Buffer(videoFilePath).toString('base64');
-return cucumberJson.attach(base64Video, 'video/mp4');
-```
-
-## Limitations
-It capture screenshots for each and every json wire protocol commands provided. Which obliviously reduces your test execution speed.
+Note:
+  `attachVideoToCucumberReport` will work only if `cucumberjs-json` reporter is enabled
---
+++ src/index.ts
@@ -0,0 +1,134 @@
+import WDIOReporter, { AfterCommandArgs, SuiteStats } from '@wdio/reporter';
+import logger from '@wdio/logger'
+import {v4 as uuidv4} from 'uuid';
+import fs from 'fs';
+import path from 'path';
+import { removeSync } from 'fs-extra/lib/remove';
+import ffmpeg from '@ffmpeg-installer/ffmpeg';
+import { execSync } from 'child_process';
+
+const log = logger('wdio-testrecorder-reporter');
+
+const DEFAULT_VIDEO_OUTPUT_FOLDER = '../../../tmp/';
+const DEFAULT_JSON_WIRE_ACTION = '../default/wdioCommand.json';
+const DEFAULT_ATTACH_VIDEO_TO_CUCUMBER_REPORT = false;
+const DEFAULT_REMOVE_ATTACHED_VIDEOS = false;
+
+let testId = null;
+let scenarioName = null;
+let imageCapturedCount = 0;
+
+export default class TestRecoder extends WDIOReporter {
+    framework: string;
+    videoOutputPath: string
+    constructor(options) {
+        super(options);
+
+        // Store video output in given path
+        if (!options.videoOutputPath) {
+            this.videoOutputPath = path.join(__dirname, DEFAULT_VIDEO_OUTPUT_FOLDER);
+            log.warn(`The 'videoOutputPath' was not set, it has been set to the default '${this.videoOutputPath}'`);
+        } else {
+            this.videoOutputPath = options.videoOutputPath;
+        }
+
+        // Determine whether to attach video to cucumber tests
+        if (typeof options.attachVideoToCucumberReport === 'boolean') {
+            if (options.attachVideoToCucumberReport) {
+                options.attachVideoToCucumberReport = options.attachVideoToCucumberReport;
+            } else {
+                options.attachVideoToCucumberReport = DEFAULT_ATTACH_VIDEO_TO_CUCUMBER_REPORT;
+            }
+        } else {
+            options.attachVideoToCucumberReport = DEFAULT_ATTACH_VIDEO_TO_CUCUMBER_REPORT;
+            log.warn(`'attachVideoToCucumberReport' received unsuported parameter, it has been set to the default '${DEFAULT_ATTACH_VIDEO_TO_CUCUMBER_REPORT}'`);
+        }
+
+        // Determine whether to attach video ahould be removed or not
+        if (typeof options.removeAttachedVideos === 'boolean') {
+            if (options.removeAttachedVideos) {
+                options.removeAttachedVideos = options.removeAttachedVideos;
+            } else {
+                options.removeAttachedVideos = DEFAULT_REMOVE_ATTACHED_VIDEOS;
+            }
+        } else {
+            options.removeAttachedVideos = DEFAULT_REMOVE_ATTACHED_VIDEOS;
+            log.warn(`'removeAttachedVideos' received unsuported parameter, it has been set to the default '${DEFAULT_REMOVE_ATTACHED_VIDEOS}'`);
+        }
+
+        options.jsonWireCommands = DEFAULT_JSON_WIRE_ACTION;
+    }
+
+    onSuiteStart(suiteStats: SuiteStats): void {
+        const suite = JSON.parse(JSON.stringify(suiteStats));
+        if (suite.type === 'scenario') {
+            testId = uuidv4();
+            scenarioName = suite.title;
+            scenarioName = scenarioName.replace(/[^a-zA-Z]/g, '_');
+            scenarioName = scenarioName.trim();
+            // initialize empty dir for storing images and videos
+            if (!fs.existsSync(`${this.videoOutputPath}`)) {
+                fs.mkdirSync(`${this.videoOutputPath}`);
+            }
+        }
+
+    }
+
+    onAfterCommand(commandArgs: AfterCommandArgs): void {
+        const command = commandArgs.endpoint.substring(commandArgs.endpoint.lastIndexOf('/') + 1).trim();
+        const actions = JSON.parse((fs.readFileSync(path.resolve(path.join(__dirname, this.options.jsonWireCommands)))).toString())
+        if (actions.jsonWireActions.includes(command)) {
+            let videoOutputPath = path.resolve(this.videoOutputPath);
+            let imageName = 'img' + imageCapturedCount.toString().padStart(3, '0');
+            let fileName = `${videoOutputPath}/${testId}-${scenarioName}/` + imageName + '.png';
+            if (testId !== null && scenarioName !== null) {
+                if (!fs.existsSync(`${videoOutputPath}/${testId}-${scenarioName}`)) {
+                    fs.mkdirSync(`${videoOutputPath}/${testId}-${scenarioName}`);
+                }
+                //@ts-expect-error
+                browser.saveScreenshot(fileName)
+                imageCapturedCount++;
+            }
+        }
+    }
+
+    onSuiteEnd(suiteStats: SuiteStats): void {
+        const suite = JSON.parse(JSON.stringify(suiteStats));
+        if (suite.type === 'scenario') {
+            let videoOutputPath = path.resolve(this.videoOutputPath);
+            let imagePath = `${videoOutputPath}/${testId}-${scenarioName}/img%03d.png`;
+            let videoPath = `${videoOutputPath}/videos/${testId}-${scenarioName}.mp4`;
+            if (!fs.existsSync(`${videoOutputPath}/videos/`)) {
+                fs.mkdirSync(`${videoOutputPath}/videos`);
+            }
+            if (fs.existsSync(`${videoPath}`)) {
+                removeSync(`${videoPath}`);
+            }
+            try {
+                const command = `${ffmpeg.path} -y -r 10 -i ${imagePath} -vcodec libx264 -crf 32 -pix_fmt yuv420p -vf "scale=1200:trunc(ow/a/2)*2","setpts=3.0*PTS" ${videoPath}`;
+                //@ts-expect-error
+                execSync(command, {stdio: 'ignore', shell: true, windowsHide: true,})
+
+                // Remove converted screenshot images
+                removeSync(`${videoOutputPath}/${testId}-${scenarioName}`);
+                
+                // Converts the recorded video to base64 string.
+                //@ts-expect-error
+                const base64Video = new Buffer.from(fs.readFileSync(videoPath)).toString('base64');
+
+                if (this.options.attachVideoToCucumberReport) {
+                    //@ts-expect-error
+                    (process.emit)( 'wdioCucumberJsReporter:attachment', { data: base64Video, type: 'video/mp4' } );
+                }
+
+                if (this.options.attachVideoToCucumberReport && this.options.removeAttachedVideos) {
+                    // Remove attached video
+                    removeSync(videoPath);
+                }
+
+            } catch (error) {
+                log.error(error);
+            }
+        }
+    }
+}
